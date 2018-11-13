aaeda8118f7177b3ab764dfb9232f325cdb54f31
Akeen11 <aartkeen@ut.utm.edu>
2018-11-13 15:19:47 -0600

    day 1 shit

---
+++ BangazonWorkforce/BangazonWorkforce/Models/Computer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BangazonWorkforce.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You must provide a purchase date for this computer.")]
+        public DateTime PurchaseDate { get; set; }
+
+        [Required(ErrorMessage = "You must provide a decomission date for this computer.")]
+        public DateTime DecomissionDate { get; set; }
+
+        [Required(ErrorMessage = "You must provide a make for this computer.")]
+        public string Make { get; set; }
+
+        [Required(ErrorMessage = "You must provide a manufacturer for this computer.")]
+        public string Manufacturer { get; set; }
+    }
+}
---
+++ BangazonWorkforce/BangazonWorkforce/Models/TrainingProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BangazonWorkforce.Models
+{
+    public class TrainingProgram
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You must provide a name for this training program.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You must provide a description for this training program.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "You must provide a start date for this training program.")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "You must provide a end date for this training program.")]
+        public DateTime EndDate { get; set; }
+
+        [Required(ErrorMessage = "You must provide an attendance number for this training program.")]
+        public int MaxAttendees { get; set; }
+    }
+}

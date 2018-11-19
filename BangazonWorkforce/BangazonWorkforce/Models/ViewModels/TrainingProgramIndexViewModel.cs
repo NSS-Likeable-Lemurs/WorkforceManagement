4d5da2ef0708df77596337c80d349e13b78deb2d
Helen Chalmers <hchalmers23@gmail.com>
2018-11-19 10:25:32 -0600

    TP Create works

---
+++ BangazonWorkforce/BangazonWorkforce/Models/ViewModels/TrainingProgramIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Author: Helen Chalmers
+//Purpose: ViewModel for the Department View
+
+namespace BangazonWorkforce.Models.ViewModels
+{
+    public class TrainingProgramIndexViewModel
+    {
+        public TrainingProgram trainingprogram { get; set; }
+    }
+}

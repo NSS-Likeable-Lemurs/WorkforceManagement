1b8591f2dae3fd508e5ecae340055b0af8e834ca
streatorward <streatorward@gmail.com>
2018-11-19 13:51:07 -0600

    working on testing for computer

---
+++ BangazonWorkforce/BangazonWorkforce.IntegrationTests/ComputerTests.cs
@@ -0,0 +1,86 @@
+﻿using AngleSharp.Dom.Html;
+using BangazonWorkforce.IntegrationTests.Helpers;
+using BangazonWorkforce.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Dapper;
+using AngleSharp.Dom;
+
+namespace BangazonWorkforce.IntegrationTests
+{
+    public class ComputerTests :
+        IClassFixture<WebApplicationFactory<BangazonWorkforce.Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public ComputerTests(WebApplicationFactory<BangazonWorkforce.Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        // Integration Testing for displaying List of computers.
+
+        [Fact]
+        public async Task Get_IndexReturnsSuccessAndCorrectContentType()
+        {
+            // Arrange
+            string url = "/computer";
+
+            // Act
+            HttpResponseMessage response = await _client.GetAsync(url);
+
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+
+            IHtmlDocument indexPage = await HtmlHelpers.GetDocumentAsync(response);
+            IHtmlCollection<IElement> tds = indexPage.QuerySelectorAll("td");
+            Assert.Contains(tds, td => td.TextContent.Trim() == "Make");
+            Assert.Contains(tds, td => td.TextContent.Trim() == "Manufacturer");
+            Assert.Contains(tds, td => td.TextContent.Trim() == "PurchaseDate");
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- BangazonWorkforce/BangazonWorkforce/Controllers/ComputerController.cs
+++ BangazonWorkforce/BangazonWorkforce/Controllers/ComputerController.cs
@@ -1,24 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonWorkforce.Models;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace BangazonWorkforce.Controllers
 {
     public class ComputerController : Controller
     {
-        // GET: Computer
-        public ActionResult Index()
+        private readonly IConfiguration _config;
+
+        public ComputerController(IConfiguration config)
         {
-            return View();
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // GET: Computer
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            string sql = $@"SELECT
+                            c.Id,
+                            c.Manufacturer,
+                            c.Make,
+                            c.PurchaseDate
+                            FROM Computer c;";
+
+            using (IDbConnection conn = Connection)
+            {
+                List<Computer> computer = (await conn.QueryAsync<Computer>(sql)).ToList();
+                return View(computer);
+            }
         }
 
         // GET: Computer/Details/5
-        public ActionResult Details(int id)
+        public async Task<IActionResult> Details(int id)
         {
-            return View();
+            string sql = $@"SELECT
+                            c.Id,
+                            c.Manufacturer,
+                            c.Make,
+                            c.PurchaseDate
+                            FROM Computer c
+                            WHERE c.Id = {id};";
+
+            using (IDbConnection conn = Connection)
+            {
+                if (CheckComputerDoesNotExist(id))
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    var computer = (await conn.QueryAsync<Computer>(sql)).Single();
+                    return View(computer);
+                }
+            }
+        }
+
+        private bool CheckComputerDoesNotExist(int id)
+        {
+            string sql = $"SELECT * FROM Computer c WHERE c.Id = {id};";
+
+            using (IDbConnection conn = Connection)
+            {
+                var theCount = conn.Query<Computer>(sql).Count();
+                return theCount == 0;
+            }
         }
 
         // GET: Computer/Create
@@ -30,18 +91,39 @@ namespace BangazonWorkforce.Controllers
         // POST: Computer/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create(Computer newComputer)
         {
-            try
-            {
-                // TODO: Add insert logic here
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
+            if (!(ModelState.IsValid))
             {
-                return View();
+                return View(newComputer);
             }
+            else
+            {
+                string sql = $@"
+				INSERT INTO Computer
+					(Manufacturer, Make, PurchaseDate)
+				VALUES
+					('{newComputer.Manufacturer}', '{newComputer.Make}', '{newComputer.PurchaseDate}')
+				";
+
+                using (IDbConnection conn = Connection)
+                {
+                    int rowsAffected = await conn.ExecuteAsync(sql);
+                    bool createdSuccessfully = rowsAffected > 0;
+
+                    if (createdSuccessfully)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        throw new Exception("No rows affected; record was not added to database");
+                    }
+                }
+            }
+
+
         }
 
         // GET: Computer/Edit/5
@@ -67,27 +149,96 @@ namespace BangazonWorkforce.Controllers
             }
         }
 
-        // GET: Computer/Delete/5
-        public ActionResult Delete(int id)
+        /* GET: Computer/Delete/5
+		 */
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            string sql = $@"
+			SELECT
+				c.Id,
+				c.Manufacturer,
+				c.Make,
+				c.PurchaseDate
+			FROM Computer c
+			WHERE c.Id = {id}
+			";
+
+            using (IDbConnection conn = Connection)
+            {
+                if (ComputerHasBeenAssigned(id))
+                {
+                    return View("DenyDelete");
+                }
+                else
+                {
+                    Computer computer = (await conn.QueryAsync<Computer>(sql)).Single();
+                    return View("Delete", computer);
+                }
+            }
         }
 
-        // POST: Computer/Delete/5
-        [HttpPost]
+        /*
+		 This method checks if a computer has already been assigned to an employee. It is used by the Delete and Delete methods.
+		 */
+        private bool ComputerHasBeenAssigned(int? id)
+        {
+            string sql = $@"
+			SELECT 
+				e.Id,
+				e.FirstName,
+				e.LastName,
+				e.DepartmentId
+			FROM Employee e
+			JOIN ComputerEmployee ec ON e.Id = ec.EmployeeId
+			JOIN Computer c ON ec.ComputerId = c.Id
+			WHERE c.Id = {id}
+			";
+
+            using (IDbConnection conn = Connection)
+            {
+                int numAssignments = (conn.Query<Employee>(sql)).Count();
+                return numAssignments > 0;
+            }
+        }
+
+        /* 
+		 POST: Computer/Delete/5
+		*/
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> Delete(int id)
         {
-            try
+            if (ComputerHasBeenAssigned(id))
             {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
+                throw new Exception("You cannot delete a computer that has been assigned to an employee");
             }
-            catch
+
+            string sql = $@"
+				DELETE FROM Computer WHERE Id = {id};
+				";
+
+            using (IDbConnection conn = Connection)
             {
-                return View();
+                int rowsAffected = await conn.ExecuteAsync(sql);
+                bool deleteSuccessful = rowsAffected > 0;
+
+                if (deleteSuccessful)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    throw new Exception("No rows affected");
+                }
             }
         }
+
+
     }
 }
